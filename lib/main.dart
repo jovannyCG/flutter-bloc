5dfd0a34667eab82bfcde7007ad7464d428d1615
Jovanny Carlos Gonzalez <jovannycarlosgonzalez9@gmail.com>
2023-04-05 12:24:21 -0600

    arreglos en el main

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/pages/page1.dart';
 import 'package:flutter_bloc/pages/page2.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -13,8 +13,8 @@ class MyApp extends StatelessWidget {
       title: 'Material App',
       initialRoute: 'pagina1',
       routes: {
-        'pagina1':(_)=>PaginaPage(),
-        'pagina2':(_)=>Pagina2Page(),
+        'pagina1':(_)=>const PaginaPage(),
+        'pagina2':(_)=>const Pagina2Page(),
       },
     );
   }
